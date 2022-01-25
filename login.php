72a6ce077e18b16aa8ec326c350453adf478f395
Klaudia164 <66058455+Klaudia164@users.noreply.github.com>
2022-01-25 08:25:29 +0100

    login

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+include 'includes/db.php';
 
 if(isset($_POST['user']) and isset($_POST['pw'])) {
 	$loginError = '';
@@ -26,6 +28,46 @@ if(isset($_POST['user']) and isset($_POST['pw'])) {
 	}
 }
 
-include 'belepes.php';
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Töltsd ki a mezőket, hogy belépj.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Felhasználónév</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Jelszó</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Még nincs felhasználód? <a href="regisztracio.php">Regisztrálj itt</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
