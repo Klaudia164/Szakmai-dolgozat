df3ff5d37965a27920e234c77e8e1adabd1d20d3
Klaudia164 <66058455+Klaudia164@users.noreply.github.com>
2022-02-02 20:35:10 +0100

    controller setup

---
+++ controller/login.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_POST['user']) and isset($_POST['pw'])) {
+	$loginError = '';
+	if(strlen($_POST['user']) == 0) $loginError .= "Nem írtál be felhasználónevet<br>";
+	if(strlen($_POST['pw']) == 0) $loginError .= "Nem írtál be jelszót<br>";
+	if($loginError == '') {
+		$sql = "SELECT id FROM felhasznalo WHERE felhasznalonev = '".$_POST['user']."' ";
+
+		if(!$result = $conn->query($sql)) echo $conn->error;
+
+		if ($result->num_rows > 0) {
+			
+			if($row = $result->fetch_assoc()) {
+				$tanulo->set_user($row['id'], $conn);
+				if(md5($_POST['pw']) == $tanulo->get_jelszo()) {
+					$_SESSION["id"] = $row['id'];
+					$_SESSION["nev"] = $tanulo->get_nev();
+                    header('Location: index.php?page=');
+                    exit();
+				}
+				else $loginError .= 'Érvénytelen jelszó<br>';
+			}
+		}
+		else $loginError .= 'Érvénytelen felhasználónév<br>';
+	}
+}
+
+include 'view/login.php';
+?>
