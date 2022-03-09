65c77f689669003f09644aab713f68bdf0e4f668
Klaudia164 <66058455+Klaudia164@users.noreply.github.com>
2022-03-09 14:04:34 +0100

    censore

--- controller/movies.php
+++ controller/movies.php
@@ -1,13 +1,20 @@
 <?php
 
 if(isset($_POST['comment'])){
-    $movies -> komment(htmlspecialchars($_POST['comment']), $_REQUEST['movieId'], $conn);
-    unset($_POST['comment']);
+    $censored=array();
+    $cfile=fopen("censored.txt",'rw');
+    while($sor = fgets($cfile)){
+        $censored[]=trim($sor);
+    }
+    fclose($cfile);
+    $comment=str_replace($censored,"***",$_POST['comment']);
+    $movies -> komment(htmlspecialchars($comment), $_REQUEST['movieId'], $conn);
+    unset($_POST);
 }
 
 if (isset($_POST['rating_data']) && isset($_SESSION['id'])){
     $movies -> set_rating($_POST['rating_data'], $_REQUEST['movieId'], $conn);
-    unset($_POST['rating_data']);
+    unset($_POST);
 }
 
 
