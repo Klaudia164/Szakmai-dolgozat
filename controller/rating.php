7f9bf22a7576e2baf8b1b457e7d4e01b14b0f82a
Klaudia164 <66058455+Klaudia164@users.noreply.github.com>
2022-03-10 11:36:14 +0100

    fix rating

--- controller/rating.php
+++ controller/rating.php
@@ -1,12 +1,44 @@
 <?php
     session_start();
     require '../model/movies.php';
+    require '../model/series.php';
+    require '../model/actors.php';
     require '../includes/db.php';
     $movies = new Movies();
-    if (isset($_POST['action'])){
+    if (isset($_POST['action']) && $_POST['action']=="movies"){
         if(isset($_SESSION['id'])){
             $output = array(
                 'rating'	=>	$movies -> get_rating($_POST['movieId'], $conn),
+            );
+            echo json_encode($output);
+        }else{
+            $output = array(
+                'rating'	=>	0,
+            );
+            echo json_encode($output);
+        }
+    }
+
+    $actors = new Actors();
+    if (isset($_POST['action']) && $_POST['action']=="actors"){
+        if(isset($_SESSION['id'])){
+            $output = array(
+                'rating'	=>	$actors -> get_rating($_POST['actorsId'], $conn),
+            );
+            echo json_encode($output);
+        }else{
+            $output = array(
+                'rating'	=>	0,
+            );
+            echo json_encode($output);
+        }
+    }
+
+    $series = new Series();
+    if (isset($_POST['action']) && $_POST['action']=="series"){
+        if(isset($_SESSION['id'])){
+            $output = array(
+                'rating'	=>	$series -> get_rating($_POST['seriesId'], $conn),
             );
             echo json_encode($output);
         }else{
